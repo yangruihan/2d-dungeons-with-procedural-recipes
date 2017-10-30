6260ac35c0e08c3082d0f878a19542e1079ede6b
C.y <yangruihan@vip.qq.com>
2017-10-30 23:43:41 +0800

    create prefabs

---
+++ Assets/Scripts/ModuleConnector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleConnector : MonoBehaviour
+{
+
+    public string[] Tags;
+    public bool IsDefault;
+
+    private void OnDrawGizmos()
+    {
+        var scale = 1f;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.right * scale);
+        Gizmos.DrawLine(transform.position, transform.position - transform.right * scale);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + transform.up * scale);
+    }
+}
